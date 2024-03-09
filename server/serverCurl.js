8b9f39492f5aae3a450a0c5c6ae2eb4d2c5c0586
Mzzqq <rezarifa661@gmail.com>
2024-03-09 18:21:04 +0700

    feat: Add HTTP server configuration

--- server/serverCurl.js
+++ server/serverCurl.js
@@ -19,3 +19,10 @@ const requestListener = (request, response) => {
     }
 }
 
+const server = http.createServer(requestListener)
+const port = 5000
+const host = "localhost"
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`)
+})
